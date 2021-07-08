5d185570b6c0e1ce4be253943009fe0255977c6d
Justin Chow <hrll71990@gmail.com>
2021-07-07 20:21:45 -0700

    upgraded to webpack 5 and webpack-cli 4 to enable use of asset modules for file resource loading

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Loading = () => {
   // need to require images for Webpack to load them
   //const loading = require('../../../public/assets/loading.svg');
-
+  //
   return (
     <div className='loading-spin'>
-      <img src={loading} alt='Loading...' />
+      <img src={'public/assets/loading.svg'} alt='Loading...' />
     </div>
   )
   
