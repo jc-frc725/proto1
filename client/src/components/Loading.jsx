b4c4a63185692d7f7bd198494dcd4a974414f9ce
Justin Chow <hrll71990@gmail.com>
2021-07-07 21:26:53 -0700

    applied asset modules for loading image assets

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 
 const Loading = () => {
   // need to require images for Webpack to load them
-  //const loading = require('../../../public/assets/loading.svg');
-  //
+  const loading = require('../../../public/assets/loading.svg');
   return (
     <div className='loading-spin'>
-      <img src={'public/assets/loading.svg'} alt='Loading...' />
+      <img src={loading} alt='Loading...' />
     </div>
   )
   
