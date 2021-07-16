5fdefd71465a3f49e50b43bcc672a5a6235d96d9
Justin Chow <hrll71990@gmail.com>
2021-07-15 21:33:15 -0700

    added Link in NavBar to Profile component

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -8,9 +8,10 @@ const NavBar = () => {
     <div className='nav-container mb-3'>
       <nav className='navbar navbar-extend-md navbar-light bg-light'>
         <div className='container'>
-          <div className='navbar-brand'>
+          <div className='navbar-brand d-flex flex-row'>
             <AuthNav />
             <Link to="/">Home</Link>
+            <Link to="/profile">Profile</Link>
           </div>
         </div>
       </nav>
